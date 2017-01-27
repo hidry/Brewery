b22deea263c12eef98bedcf21bb36b3b77e8d5ac
hidry <hidry@gmx.de>
2017-01-26 20:03:03 -0800

    mixer-control with Gpio12

--- Brewery/Brewery/MainPage.xaml.cs
+++ Brewery/Brewery/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,18 +24,54 @@ namespace Brewery
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private bool _mixerActive = false;
+        private GpioController _gpioController;
+        private GpioPin _gpio12; //pin 32
+
         public MainPage()
         {
             this.InitializeComponent();
+            
             RefreshDateTime(null, null);
             var timer = new DispatcherTimer() { Interval = new TimeSpan(0,0,1) };
             timer.Tick += RefreshDateTime;
             timer.Start();
+
+            _gpioController = GpioController.GetDefault();
+
+            if (_gpioController == null)
+            {
+                throw new Exception("There is no GPIO controller on this device.");
+            }
+
+            InitGpio12();
+        }
+
+        private void InitGpio12()
+        {
+            _gpio12 = _gpioController.OpenPin(12);
+            _gpio12.Write(GpioPinValue.Low);
+            _gpio12.SetDriveMode(GpioPinDriveMode.Output);
         }
 
         private void RefreshDateTime(object sender, object e)
         {
-            DateTime.Text = System.DateTime.Now.ToString();
+            DateTimeTextBlock.Text = DateTime.Now.ToString();
+        }
+
+        private void MixerHyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_mixerActive)
+            {
+                _gpio12.Write(GpioPinValue.Low);
+                MixerTextBlock.Text = "Aus";
+            }
+            else
+            {
+                _gpio12.Write(GpioPinValue.High);
+                MixerTextBlock.Text = "An";
+            }            
+            _mixerActive = !_mixerActive;
         }
     }
 }
