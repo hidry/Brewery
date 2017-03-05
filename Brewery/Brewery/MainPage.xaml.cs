3fdd0d58975919d66692b7aa98884d9781dc0ec6
Stefan Reinbold <hidry@gmx.de>
2017-03-05 16:35:22 +0100

    - NuGet packages update - Rinsen.OneWire added - Temperature measurement added

--- Brewery/Brewery/MainPage.xaml.cs
+++ Brewery/Brewery/MainPage.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Windows.Devices.Gpio;
+using Rinsen.IoT.OneWire;
+using System.Diagnostics;
+using System.Linq;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,13 +19,15 @@ namespace Brewery
         private bool _mixerActive = false;
         private GpioController _gpioController;
         private GpioPin _gpio12; //pin 32
+        private OneWireDeviceHandler _handler;
+        private IEnumerable<DS18B20> _devices;
 
         public MainPage()
         {
-            this.InitializeComponent();
-            
+            InitializeComponent();
+
             RefreshDateTime(null, null);
-            var timer = new DispatcherTimer() { Interval = new TimeSpan(0,0,1) };
+            var timer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
             timer.Tick += RefreshDateTime;
             timer.Start();
 
@@ -41,10 +35,31 @@ namespace Brewery
 
             if (_gpioController == null)
             {
-                throw new Exception("There is no GPIO controller on this device.");
+                Debug.WriteLine("No gpio controller available.");
+            }
+            else
+            {
+                InitGpio12();
             }
 
-            InitGpio12();
+            _handler = new OneWireDeviceHandler(false, false); //todo: Dispose
+            _devices = _handler.OneWireDevices.GetDevices<DS18B20>();
+            if (_devices == null || !_devices.Any())
+            {
+                Debug.WriteLine("No temperature sensor (DS18B20) available.");
+            }
+            else
+            {
+                var temperatureTimer = new DispatcherTimer() { Interval = new TimeSpan(0, 0, 1) };
+                temperatureTimer.Tick += RefreshTemperature;
+                temperatureTimer.Start();
+            }
+        }
+
+        private void RefreshTemperature(object sender, object e)
+        {
+            var result = _devices.First().GetTemperature();
+            TemperatureTextBlock.Text = result.ToString();
         }
 
         private void InitGpio12()
@@ -63,14 +78,22 @@ namespace Brewery
         {
             if (_mixerActive)
             {
-                _gpio12.Write(GpioPinValue.Low);
+                if (_gpio12 != null)
+                {
+                    _gpio12.Write(GpioPinValue.Low);
+                }
                 MixerTextBlock.Text = "Aus";
+                MixerProgressRing.IsActive = false;
             }
             else
             {
-                _gpio12.Write(GpioPinValue.High);
+                if (_gpio12 != null)
+                {
+                    _gpio12.Write(GpioPinValue.High);
+                }
                 MixerTextBlock.Text = "An";
-            }            
+                MixerProgressRing.IsActive = true;
+            }
             _mixerActive = !_mixerActive;
         }
     }
