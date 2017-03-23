2c4f1ba2c0c31a36aaf0e4ef0307664d26b671ab
Stefan Reinbold <hidry@gmx.de>
2017-03-23 16:28:35 +0100

    - WebViewer added

--- Brewery/Brewery/MainPage.xaml.cs
+++ Brewery/Brewery/MainPage.xaml.cs
@@ -39,9 +39,11 @@ namespace Brewery
 
         public static List<MenuItem> GetMainItems()
         {
+            //todo: texte aus resource
             var items = new List<MenuItem>();
-            items.Add(new MenuItem() { Icon = Symbol.Bold, Name = "Brauprozess", PageType = typeof(Views.BrewProcess) }); //todo: resource
-            items.Add(new MenuItem() { Icon = Symbol.Setting, Name = "Einstellungen", PageType = typeof(Views.Settings) }); //todo: resource
+            items.Add(new MenuItem() { Icon = Symbol.Bold, Name = "Brauprozess", PageType = typeof(Views.BrewProcess) });
+            items.Add(new MenuItem() { Icon = Symbol.Globe, Name = "Website", PageType = typeof(Views.WebViewer) });
+            items.Add(new MenuItem() { Icon = Symbol.Setting, Name = "Einstellungen", PageType = typeof(Views.Settings) });
             return items;
         }
 
---
+++ Brewery/Brewery/Views/WebViewer.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace Brewery.Views
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class WebViewer : Page
+    {
+        public WebViewer()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GoToUrl(object sender, RoutedEventArgs routedEventArgs)
+        {
+            WebView.Navigate(new Uri(UrlTextBox.Text));
+        }
+    }
+}
